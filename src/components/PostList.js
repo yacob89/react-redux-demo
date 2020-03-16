038cf5481235f291f480e0ac3978ec2e617171f6
Yacob Madiana <Yacob_M156@mitrais.com>
2020-03-16 14:07:45 +0700

    Add Header Reducers

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -12,8 +13,11 @@ class PostList extends React.Component {
         <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
           <div className="content">
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
